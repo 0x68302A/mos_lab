45d97754a4662043cec9f606096cfc8d9ccae874
Aran Ailbhe <mosp08@protonmail.com>
2022-03-11 01:06:01 +0200

    Create a basic nftables based firewall- Allowing for secure- full interconnection, and limiting access beyond the bridge subnet.
    *Not sure of the safety of such a design, yet.

--- hooks/nftables.sh
+++ hooks/nftables.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-export default_gw=$(echo $(ip route | grep default) | cut -d " " -f 3)
-export iface_nic="mos_sec-pub"
-
 ## Create Base for our firewall
-nft "flush ruleset"
-nft "add table FILTER"
-nft "add chain FILTER INPUT { type filter hook input priority 0; policy accept; }"
-nft "add rule FILTER INPUT ct state established,related accept;"
+sudo nft "flush ruleset"
 
-## Enable IPV4 forwarding
-sysctl -q net.ipv4.ip_forward=0
-sysctl -q net.ipv6.conf.default.forwarding=0
-sysctl -q net.ipv6.conf.all.forwarding=0
+sudo nft "add table mos_lab"
+sudo nft "add chain mos_lab input { type filter hook input priority 0; policy accept;} "
+sudo nft "add rule mos_lab input ct state established,related accept;"
+sudo nft "add rule mos_lab input iifname "mos_lab-priv" drop;"
+
+## Accept inter-vm connections
+sudo nft "add rule mos_lab input ip daddr 192.168.122.0/24 accept;"
+sudo nft "add rule mos_lab input ip daddr 192.168.122.1 drop;"
+
+## Accept SSH related connections
+sudo nft "add rule mos_lab input iifname "mos_lab-priv" tcp dport 2022 accept;"
+
+
