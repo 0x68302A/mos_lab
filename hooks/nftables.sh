67545e5f16337ff2d7e384e1497f50a45e5ac303
Aran Ailbhe <mosp08@protonmail.com>
2022-03-19 11:43:55 +0200

    Add needed network interfaces, and nftables firewall configuration- for the new target ( b38d413 ) and further secure the firewall.

--- hooks/nftables.sh
+++ hooks/nftables.sh
@@ -1,18 +1,65 @@
 #!/bin/bash
 
-## Create Base for our firewall
+## Export interface IDs
+export default_gw=$(echo $(ip route | grep default) | cut -d " " -f 3)
+export default_iface=$(/sbin/route | grep '^default' | grep -o '[^ ]*$')
+
+export iface_pub_nic="mos_lab-pub"
+
+####################
+## GENERAL RULLES ##
+####################
+
 sudo nft "flush ruleset"
 
-sudo nft "add table mos_lab"
-sudo nft "add chain mos_lab input { type filter hook input priority 0; policy accept;} "
-sudo nft "add rule mos_lab input ct state established,related accept;"
-sudo nft "add rule mos_lab input iifname "mos_lab-priv" drop;"
+## Create table, and accept established,related packets
+sudo nft "add table mos_lab-filter"
+sudo nft "add chain mos_lab-filter input { type filter hook input priority 0; policy accept; } "
+sudo nft "add rule mos_lab-filter input ct state established,related accept;"
 
-## Accept inter-vm connections
-sudo nft "add rule mos_lab input ip daddr 192.168.122.0/24 accept;"
-sudo nft "add rule mos_lab input ip daddr 192.168.122.1 drop;"
+## Drop all traffic from the private ( Potetianly malicious ) interfaces
+sudo nft "add rule mos_lab-filter input iifname mos_lab-priv-01 drop;"
+sudo nft "add rule mos_lab-filter input iifname mos_lab-priv-02 drop;"
+sudo nft "add rule mos_lab-filter input iif " $default_iface " accept;"
 
-## Accept SSH related connections
-sudo nft "add rule mos_lab input iifname "mos_lab-priv" tcp dport 2022 accept;"
+## Enable IPV4 forwarding
+sudo sysctl -q net.ipv4.ip_forward=1
+sudo sysctl -q net.ipv6.conf.default.forwarding=0
+sudo sysctl -q net.ipv6.conf.all.forwarding=0
 
 
+######################
+## PUBLIC INTERFACE ##
+######################
+
+## Create table, and accept established,related packets
+sudo nft "add table mos_lab-pub-nat"
+sudo nft "add chain mos_lab-pub-nat mos_prerouting { type nat hook prerouting priority -100; policy drop; }"
+
+## Configure NAT options, for our host- connected bridge interface
+sudo nft "add chain mos_lab-pub-nat mos_postrouting { type nat hook postrouting priority 100 ; }"
+sudo nft "add rule mos_lab-pub-nat mos_postrouting masquerade;"
+
+## Configure vm to host relationship
+sudo nft "add rule mos_lab-pub-nat mos_prerouting iif" $iface_pub_nic "accept;"
+sudo nft "add rule mos_lab-pub-nat mos_prerouting iif" $iface_pub_nic "tcp dport { 80, 443 } dnat " $default_gw ";"
+
+
+########################
+## PRIVATE INTERFACEs ##
+########################
+
+## Create table, and accept established,related packets
+sudo nft "add table mos_lab-priv"
+sudo nft "add chain mos_lab-priv mos_input { type filter hook input priority 50; policy drop; } "
+sudo nft "add rule mos_lab-priv mos_input ct state established,related accept;"
+
+## Accept inter-vm connections on priv-01 bridge, drop all else
+sudo nft "add rule mos_lab-priv mos_input iifname mos_lab-priv-02 drop;"
+sudo nft "add rule mos_lab-priv mos_input iifname mos_lab-priv-02 ip daddr 10.0.4.0/24 accept;"
+sudo nft "add rule mos_lab-priv mos_input iifname mos_lab-priv-02 ip daddr 10.0.4.1 drop;"
+
+## Accept inter-vm connections on priv-02 bridge, drop all else
+sudo nft "add rule mos_lab-priv mos_input iifname mos_lab-priv-01 drop;"
+sudo nft "add rule mos_lab-priv mos_input iifname mos_lab-priv-01 ip daddr 10.0.2.0/24 accept;"
+sudo nft "add rule mos_lab-priv mos_input iifname mos_lab-priv-01 ip daddr 10.0.2.1 drop;"
